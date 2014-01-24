955e4eeea2f4d794982b94f0ed960224cf3b71a3
brunorovs <bruno.rovea@live.com>
2014-01-24 10:00:33 -0200

    oi

---
+++ WeHelp/WeHelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeHelp
+//
+//  Created by Bruno Rovea Soares on 24/01/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeHelp/WeHelp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeHelp
+//
+//  Created by Bruno Rovea Soares on 24/01/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
