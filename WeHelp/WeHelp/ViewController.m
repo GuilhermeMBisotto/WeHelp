10e8fd9a3345a1f61b154160bd7faa192a12cde4
Guilherme Moresco Bisotto <guilhermemorescobisotto@Guilhermes-MacBook-Pro.local>
2014-01-27 09:34:50 -0200

    Iniciando criação da Home.

--- WeHelp/WeHelp/ViewController.m
+++ WeHelp/WeHelp/ViewController.m
@@ -7,24 +7,19 @@
 //
 
 #import "ViewController.h"
+#define Rgb2UIColor(r, g, b)[UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:1.0]
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *oi;
-
 @end
 
 @implementation ViewController
-- (IBAction)heu:(id)sender {
-    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"alert" message:@"Sauna Hour \o/" delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:@"Cancel", nil];
-    [alert show];
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    
+    UIColor *navColor = [UIColor colorWithRed:((14) / 255.0) green:((170) / 255.0) blue:((237) / 255.0) alpha:1.0];
+    self.navigationController.navigationBar.barTintColor = navColor;
 }
 
 - (void)didReceiveMemoryWarning
